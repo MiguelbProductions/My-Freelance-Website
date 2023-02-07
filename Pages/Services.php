c0e73253aa14e931b3cf6f805c7a549c83fcaf5e
Miguel Brandão <miguellbproductions@gmail.com>
2023-02-07 23:45:27 +0000

    Service Page added

---
+++ Pages/Services.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home | Miguel and Alan</title>
+
+        <link rel="stylesheet" href="../Style/Services.css">
+        
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
+        </style>
+    </head>
+    <body>
+        <?php include "Header.php" ?>
+
+        <h1>
+            Services
+        </h1>
+
+        <div id="apresentation-text">
+            <p>
+                Some of the services we perform for you: <br>
+                (if you need another one that is not listed here, please contact us)
+            </p>
+        </div>
+
+        <div id="themes-content">
+            <div class="theme-box">
+                <i class="fa-solid fa-pen-nib fa-4x"></i> <br>
+
+                <span class="text">
+                    WEB DESIGN
+                </span>
+                <div class="bar"></div>
+            </div>
+            <div class="theme-box">
+            <i class="fa-solid fa-code fa-4x"></i> <br>
+
+                <span class="text">
+                    WEB DEVELPMENT
+                </span>
+                <div class="bar"></div>
+            </div>
+            <div class="theme-box">
+                <i class="fa-solid fa-mobile-screen-button fa-4x"></i> <br>
+
+                <span class="text">
+                    APP DEVELOPMENT
+                </span>
+                <div class="bar"></div>
+            </div>
+            <div class="theme-box">
+                <i class="fa-sharp fa-solid fa-message-bot fa-4x"></i> <br>
+
+                <span class="text">
+                    BOTS
+                </span>
+                <div class="bar"></div>
+            </div>
+            <div class="theme-box">
+            <i class="fa-solid fa-code-branch fa-4x"></i> <br>
+
+                <span class="text">
+                    API INTEGRATION
+                </span>
+                <div class="bar"></div>
+            </div>
+            <div class="theme-box">
+            <i class="fa-solid fa-gamepad-modern fa-4x"></i> <br>
+
+                <span class="text">
+                    GAME DEVELOPMENT
+                </span>
+                <div class="bar"></div>
+            </div>
+        </div>
+
+    </body>
+</html>
